40a2233da1b0b5a60d19cfbcf41d79cb23de34c3
stonear <27681405+stonear@users.noreply.github.com>
2018-11-15 22:32:12 +0700

    init

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  var random = new TimeSeries();
+
+  var chart = new SmoothieChart({
+    maxValue:10,
+    minValue:-10,
+    tooltip:true,
+    // timestampFormatter:SmoothieChart.timeFormatter
+  });
+  chart.addTimeSeries(random, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+  // delay 3 detik
+  chart.streamTo(document.getElementById("chart"), 3000);
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA88V9rTa-pQVvKR4tJckJ7GHtNH9uEdEY",
+    authDomain: "chart-b92ce.firebaseapp.com",
+    databaseURL: "https://chart-b92ce.firebaseio.com",
+    projectId: "chart-b92ce",
+    storageBucket: "chart-b92ce.appspot.com",
+    messagingSenderId: "203145862827"
+  };
+  firebase.initializeApp(config);
+
+  d = $("#data");
+  const dbRef = firebase.database().ref().child("data");
+  dbRef.on("child_added", function(snapshot, previousChildKey) {
+    console.log(snapshot.val());
+    d.text(snapshot.val().value);
+
+    // var waktu = new Date();
+    // random.append(waktu.getTime(), snapshot.val().value);
+    // console.log(waktu.toTimeString());
+
+    var waktu = new Date(snapshot.val().date + "T" + snapshot.val().time + "+07:00");
+    random.append(waktu.getTime(), snapshot.val().value);
+    console.log(waktu.toTimeString());
+  });
+});
